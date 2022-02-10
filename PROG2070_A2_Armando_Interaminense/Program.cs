2ed0892945a96f33f65f1f3cb86dabd5ed4dc790
Armando Interaminense <amirandainteram6730@conestogac.on.ca>
2022-02-10 16:47:01 -0500

    Completed Rectange Class

--- PROG2070_A2_Armando_Interaminense/Program.cs
+++ PROG2070_A2_Armando_Interaminense/Program.cs
@@ -4,12 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace PROG2070_A2_Armando_Interaminense
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Rectangle rect = new Rectangle(2,3);
+            Console.WriteLine("The length is: {0}cm\nThe width is: {1}cm" +
+                "\nThe perimeter is: {2}cm\nThe area is: {3}cm\u00b2",rect.GetLength(),rect.GetWidth(),
+                rect.GetPerimeter(),rect.GetArea());
+            Console.Read();
         }
     }
 }
---
+++ PROG2070_A2_Armando_Interaminense/Rectangle.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG2070_A2_Armando_Interaminense
+{
+    /// <summary>
+    /// Define a class for rectangles and its properties
+    /// such as, width, length, perimeter, and length
+    /// </summary>
+    public class Rectangle
+    {
+        //private properties
+        private int width;
+        private int length;
+
+        //default constructor
+        public Rectangle()
+        {
+            width = 1;
+            length = 1;
+        }
+
+        //constructor
+        public Rectangle(int width, int length)
+        {
+            this.width = width;
+            this.length = length;
+        }
+
+        //methods
+        /// <summary>
+        /// Get the length of the rectangle
+        /// </summary>
+        /// <returns>length of the rectangle</returns>
+        public int GetLength() { return length; }
+        /// <summary>
+        /// Set a length for the rectangle
+        /// </summary>
+        /// <param name="length">new length for the rectangle</param>
+        /// <returns>the length assignment statement</returns>
+        public int SetLength(int length) { return this.length=length; }
+        /// <summary>
+        /// Get the width of the rectangle
+        /// </summary>
+        /// <returns>width of the rectangle</returns>
+        public int GetWidth() { return width; }
+        /// <summary>
+        /// Set a width for the rectangle
+        /// </summary>
+        /// <param name="width">new width for the rectangle</param>
+        /// <returns>the width assignment statement</returns>
+        public int SetWidth(int width) { return this.width = width;}
+        /// <summary>
+        /// Calculate the perimeter of the rectangle
+        /// </summary>
+        /// <returns>The perimeter of the rectangle</returns>
+        public int GetPerimeter() { return 2*(width+length); }
+        /// <summary>
+        /// Calculate the area of the rectangle
+        /// </summary>
+        /// <returns>The area of the rectangle</returns>
+        public int GetArea() { return width*length;}
+
+
+
+    }
+}
